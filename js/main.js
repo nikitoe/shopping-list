6a67e72aad0e897ef97f48bea9e422fbad60bbcf
Jiyong Sim <sjy19910222@gmail.com>
2022-02-11 01:21:19 +0900

    JS: Add function
    1. Add input's enter function
    2. Scroll to the newly added item.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
@@ -13,7 +15,9 @@ function onAdd() {
     const item = createItem(text);
     // 3. items 컨테이너안에 새로 만든 아이템을 추가한다.
     items.appendChild(item);
-    // 4. input을 초기화 한다.
+    // 4. 새로 추가된 아이템으로 스크롤링
+    item.scrollIntoView({block:'center'});
+    // 5. input을 초기화 한다.
     input.value = '';
     input.focus();
 }
@@ -53,5 +57,7 @@ addBtn.addEventListener('click', () => {
 
 
 input.addEventListener('keypress', (event) => {
-    console.log(event);
+    if (event.key === 'Enter') {
+        onAdd();
+    }
 });
